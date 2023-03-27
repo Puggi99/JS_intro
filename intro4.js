b49adcd61cf688d1d89f9e79312b5560c9b9dac7
Puggi99 <127298032+Puggi99@users.noreply.github.com>
2023-03-27 13:33:10 +0200

    update

--- intro4.js
+++ intro4.js
@@ -2,7 +2,7 @@
 
 // L'Array è una scatola che, invece di contenere una cosa sola, può conterne quante ne vuole, e si scrive così:
 
-let pippo = [15, 3, 23, 34, 21];
+// let pippo = [15, 3, 23, 34, 21];
 
 // let pluto = [1, "ciao", true];                          // In uno stesso Array possono essere presenti elementi di vario tipo, però è sconsigliabile utilizzare questo metodo perchè può causare problemi.
                                                            // Per questo motivo è meglio creare un Array per ogni tipo (uno per stringhe, uno per numeri ecc..).
@@ -41,11 +41,18 @@ let pippo = [15, 3, 23, 34, 21];
 
 // COME CICLCARE DENTRO UN ARRAY
 
-// const arrayOfStrings = ["Andrea", "Jing", "Valentina", "Vlad"]
+// const arrayOfStrings = ["Andrea", "Davide", "Jing", "Valentina", "Vlad"]
 // for (let i = 0; i < arrayOfStrings.length; i++) {
 //     const element = arrayOfStrings[i];
 //     console.log(element);
 // }
+
+// const indexOfJing = arrayOfStrings.indexOf('Jing');
+// console.log(indexOfJing)
+
+// arrayOfStrings[0] = "Pietro";
+
+// console.log(arrayOfStrings[0]);
 
 
 
@@ -81,7 +88,7 @@ let pippo = [15, 3, 23, 34, 21];
 
 // function smallWords(text){
 //     for (let i = 0; i < words.length; i++) {
-//         let element = words[i];
+//         const element = words[i];
 //     if(element.length < 5){
 //         console.log(element);
 //     }
@@ -100,20 +107,23 @@ let pippo = [15, 3, 23, 34, 21];
 //     return text;    
 // }
 
-// let newText ="";
+
 // function firstFiveChars(text){
-//     for (let i = 0; i < 5; i++) {
+//     let newText ="";
+//     for (let i = 0; i < text.length; i++) {
+//         if (i < 5){
 //         const element = fiveCharsUpperCase(text[i]);
 //         newText += element;
         
-//     } return newText;
-
+//     }
+//     }return newText;
 
 // } console.log(firstFiveChars("tomareomo"))
 
 
 // (4) Scrivere una funzione che prenda come paramentro una stringa e restituisca la lunghezza della stringa senza spazi.
 
+// 1° soluzione
 
 // function stringLengthNoSpaces(randomWord){
 //     let count = 0;
@@ -123,6 +133,13 @@ let pippo = [15, 3, 23, 34, 21];
 //         }
 //     }return count;
 // } console.log(stringLengthNoSpaces("c         iao"));
+
+// 2° soluzione
+
+// function lengthOfTheTextWithoutSpaces(text){
+//     const textWithoutSpaces = text.replaceAll(" ", "");
+//     return textWithoutSpaces.length;
+// }
 
 
 
@@ -170,36 +187,299 @@ let pippo = [15, 3, 23, 34, 21];
 
 
 // CORREGGERE PROBLEMATICA FUNZIONE CON NUMERI NEGATIVI
-function isMyNumberNegative(selectedNumber){
-    if(selectedNumber < 0){
-        selectedNumber *= -1;
+// function isMyNumberNegative(selectedNumber){
+//     if(selectedNumber < 0){
+//         selectedNumber *= -1;
     
-    }const newNumber = selectedNumber;
-     return !isMyNumberOdd(newNumber); 
+//     }const newNumber = selectedNumber;
+//      return !isMyNumberOdd(newNumber); 
+// }
+
+// function isMyNumberEven(selectedNumber){
+//     if(selectedNumber === 0){
+//         return true;
+//     }
+//     if (selectedNumber === 1) {
+//         return false;
+//     }
+//     const newNumber = selectedNumber - 2
+//     return !isMyNumberNegative(newNumber); 
+// }
+
+
+// function isMyNumberOdd(selectedNumber){
+//     if(selectedNumber === 0){
+//         return false;
+//     }
+//     if (selectedNumber === 1) {
+//         return true;
+//     }
+//     const newNumber = selectedNumber - 2
+//     return !isMyNumberEven(newNumber); 
+// };
+
+// console.log(isMyNumberEven(3)); //false
+// console.log(isMyNumberOdd(-2)); //true
+
+
+
+// _____________________________________________________________________________________________________________
+
+
+// OBJECTS / OGGETTI
+
+// Si indica con le graffe. Serve a contenere tipologie di dati diversi. A differenza dell'Array che contiene tanti dati, ma di tipologia uguale.
+
+// const Andrea = {
+
+//     name: "Andrea",
+//     surname: "Asioli",
+//     yob: 1978,
+//     isMarried: false,
+//     pupils:["Andrea", "Davide", "Jing", "Valentina", "Vlad"],
+//     dog:{
+//         name: "ariel",
+//         breed: "border-collie",
+//     },
+//     "cool car": "BMW",                 // se c'è un nome con lo spazio, o comunque nomi particolari, vanno usate le "".
+// }   
+// console.log(Andrea)
+// console.log(Andrea.surname)            // per richiamare un dato presente nell'object, basta usare questa sintassi. (Usare il ".").
+// console.log(Andrea["yob"])             // oppure usare le parentesi quadre con le virgolette.
+
+
+
+
+// USARE OBJECTS E ARRAY INSIEME.
+
+// const Daniele = {
+//     name:"Daniele",
+//     surname: "Puggioni",
+//     nickname: "Pooh",
+//     "e-mail":  "danielepuggioni@hotmail.it",
+// }
+
+// const myClass = [Daniele];
+// console.log(myClass);
+
+
+// for(let i = 0; i < myClass.length; i++){
+//     const person = myClass[i];
+//     const description = {
+//             'name': {$(person.name}; ${person.surname}; {$(person.nickname)},
+//             ETA: $(2023 - person.yob),
+//             Email: (person.e-mail),
+//             console.log(description)
+//     }
+
+// }
+
+
+
+
+// DA ARRAY A OGGETTI E VICEVERSA
+
+// const arielArray = ['ariel', 'border-collie', 'maschio', 'bianco e nero'];
+
+// const arielobj = {
+//     name: arielArray[0],                                      // name = key, arielArray = value
+//     breed:arielArray[1],
+//     gender:arielArray[2],
+//     color: arielArray[3],
+// }
+
+// console.log(arielArray);
+// console.log(arielobj);
+
+
+// const newArielArray = Object.values(arielobj);
+// console.log(newArielArray)
+
+// const newKeysArray = Object.keys(arielobj);
+// console.log(newKeysArray);
+
+
+
+// VALUES TYPES AND REFERENCE TYPES
+
+// let pippo = 5;
+// let pluto = pippo;
+// pippo = 7;
+// console.log(pippo);
+// console.log(pluto);
+
+// let topolino = {yob: 1930, name: "mickey mouse"};
+// let newtopolino = topolino;
+// topolino.yob = 1950;
+// console.log(topolino.yob);
+// console.log(newtopolino.yob);
+
+// function sumTwo(selectedNumber){
+//      selectedNumber += 2;
+//      return selectedNumber; 
+// }
+
+// let paperino = 5;
+// let basettoni = sumTwo(paperino);
+// console.log(paperino);
+// console.log(basettoni);
+
+// function makeTwoYearsOlder(person){
+//     person.yob += 2;
+//     return person;
+// }
+
+// let Andrea = {
+//     name: "Andrea",
+//     yob: 1978,
+// }
+
+// let olderAndrea = makeTwoYearsOlder(Andrea);
+// console.log(olderAndrea)
+// console.log(Andrea)
+
+// // #creare un clone
+
+// function makeTwoYearsOlderWithClone(person){
+//     // let clone = {name : person.name, yob: person.yob}                        // per clonare velocemente tutte le caratteristiche, basta usare "...
+//     let clone = {...person}
+//     clone.yob = clone.yob +2;
+//     return clone;
+// }
+
+// let simone = {name: "simone", yob: 2003};
+// let olderSimone = makeTwoYearsOlderWithClone(simone);
+// console.log(simone);
+// console.log(olderSimone);
+
+
+// let clone = {...person }
+
+
+// DESTRUTTTURARE 
+
+// const libro = {
+//     title: "La divina commedia",
+//     author: "Dante Alighieri",
+//     year: 2019,
+//     state: "Italy",
+//     editor: "Einaudi"
+// }
+
+// const title = (libro.title);
+// const author = (libro.author);
+
+// invece che scrivere queste due righe, è possibile fare così:
+
+// const {title, author} = libro;
+// console.log(title,author)
+
+
+
+
+// JSON
+
+// E' nato per avere un metodo veloce di conversione di un array in una stringa, e poi dalla stringa in object.
+
+// const libro = {
+//     title: "La divina commedia",
+//     author: "Dante Alighieri",
+//     year: 2019,
+//     state: "Italy",
+//     editor: "Einaudi"
+// }
+
+
+// const libroInStringa = JSON.stringify(libro);                       // comando JSON.stringify per trasformare in stringa
+// console.log(libro);                                                 // comando JSON.parse per trasformare in object
+// console.log(libroInStringa);
+
+// const libroInStringa2 = '{"title":"Iliade","author":"Omero","year":2019,"state":"Italy","editor":"Giunti"}'
+// const libro2 = JSON.parse(libroInStringa2)
+// console.log(libro2)
+
+
+
+// _____________________________________________________________________________________________________________
+
+// Esempi Array
+
+
+// FILTER
+
+//1) scrivere una funzione che dato un array di numeri restituisca un array con i numeri maggiori o uguali a  0
+
+// const numbers = [100, -23, 0, -234, 1001, 1, 34, -12]
+// function interNegative(array){
+//     let tempArray=[]
+//     for (let i = 0; i < numbers.length; i++){
+//         const element = numbers[i];
+//         if(element >= 0){
+//           tempArray.push(element);
+//         }
+
+//     }return tempArray;
+// }
+
+// console.log(interNegative(numbers)) // 100, 0, 1001, 1, 34.
+
+
+
+// MAPPING (Cambiare elementi degli Array)
+
+//(2) Scrivere una funzione che dato un array di numeri li moltiplichi tutti per 2
+
+// const numbers = [100, -23, 0, -234, 1001, 1, 34, -12]
+
+// function multiplyAllElementsByTwo(array){
+//     let tempArray = []
+//     for (let i = 0; i < array.length; i++) {
+//         const element = array[i]
+//         let risultato = element *2;
+//         tempArray.push(risultato);
+//     }return tempArray;
+// }
+// console.log(multiplyAllElementsByTwo(numbers)); // 200, -46, 0, -468, 2002, 2, 68, -24;
+
+
+// REDUCE (Ridurre gli elementi ad un unico valore)
+
+//(3) Creare una funzione che somma tutti gli elementi di un array di numeri 
+
+// const numbers = [100, -23, 0, -234, 1001, 1, 34, -12]
+
+// function sumAllElements(array){
+//     let newNumber = 0
+//     for (let i = 0; i < array.length; i++) {
+//         const element = array[i];
+//         newNumber = newNumber + element;
+//     }
+//     return newNumber
+// }
+// console.log(sumAllElements(numbers)); //  867
+
+
+
+// _____________________________________________________________________________________________________________________________________________________
+
+
+//                                                                      ESERCIZI
+
+//(1) Scrivere una funzione che prende in input un array di numeri e restituisce un array di stringhe in logica fizzBuzz
+const numbers = [3,5,15,8,12,2]
+function convertiArrayInFizzBuzz(array){
+
 }
 
-function isMyNumberEven(selectedNumber){
-    if(selectedNumber === 0){
-        return true;
-    }
-    if (selectedNumber === 1) {
-        return false;
-    }
-    const newNumber = selectedNumber - 2
-    return !isMyNumberNegative(newNumber); 
+console.log(convertiArrayInFizzBuzz(array)); // ['Fizz','Buzz','FizzBuzz','8','Fizz','2']
+//(2) Scrivere una funzione che prende in input un array di numeri e restituisce un array di numeri in cui i positivi sono trasformati in negativi e viceversa
+const numbers = [-3,5,15,-8,12,2,0]
+function invertiElements(array){
+
 }
-
-
-function isMyNumberOdd(selectedNumber){
-    if(selectedNumber === 0){
-        return false;
-    }
-    if (selectedNumber === 1) {
-        return true;
-    }
-    const newNumber = selectedNumber - 2
-    return !isMyNumberEven(newNumber); 
-};
-
-console.log(isMyNumberEven(3)); //false
-console.log(isMyNumberOdd(-2)); //true
+console.log(invertiElements(numbers)); // [3,-5,-15,8,-12,-2,0]
+//(3) Scrivere una funzione che prende in input un array di stringhe e restuisce un array di numeri con la lunghezza delle stringhe 
+//(4) Scrivere una funzione che prende in input un array di stringhe e restisuice solo quelle che contengono la lettera z
+//(5) Scrivere una funzione che prende in input un array di numeri e restituisce tutti i numeri pari 
+//(6) Scrivere una funzione che dato un array di stringhe restituisca una stringa composta dalle iniziali 
+//(7) Scrivere una funzione che prende un array di numeri e restituisce il maggiore 
